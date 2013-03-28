52a6d6ccf9fd9ea3e6a66852409a6474bbd0d96f
David Dias <daviddias.p@gmail.com>
2013-03-28 17:01:44 +0000

    advancing

--- src/NewsCollectionAndStorage.py
+++
@@ -1,9 +0,0 @@
-import feedparser
-from whoosh.fields import *
-from whoosh.index import *
-
-
-dn = feedparser.parse("http://feeds.dn.pt/DN-Politica")
-jn = feedparser.parse("http://feeds.jn.pt/JN-Politica")
-
-
