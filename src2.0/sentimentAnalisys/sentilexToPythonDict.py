a107f8be706947a0d5413acae288ace857c71ed2
David Dias <daviddias.p@gmail.com>
2013-04-18 08:10:12 +0100

    something

--- src2.0/sentimentAnalisys/sentilexToPythonDict.py
+++ src2.0/sentimentAnalisys/sentilexToPythonDict.py
@@ -1,4 +1,5 @@
 import nltk
+import codecs
 
 def sentilexToPython():
     palavras = {}
@@ -7,38 +8,35 @@ def sentilexToPython():
     neutro = 0
 
     fileName = "SentiLex-flex-PT01.txt"
-    inputFile = open(fileName,"rb")
-    i = "abelhudo"
-    text = "Antonio Joss Seguro ja leu a carta e prosseguiu a agenda do dia De momento nada mais ha a acrescentar disse a mesma fonte da direcao do PS fazendo contudo a seguinte ressalva Mas seria a primeira vez que um lider da oposicao nao estaria num encontro apos convite do primeiro ministro O primeiro ministro Pedro Passos Coelho convidou hoje o secretario geral do PS para um encontro a realizar na quarta feira de manha tendo em vista um entendimento sobre as medidas para a consolidacao orcamental Em particular torna se urgente garantir o cumprimento da execucao orcamental de 2013 e do quadro orcamental de medio prazo e concertar as medidas que garantam esse objetivo dado ate que algumas delas se encontram para alem do prazo da legislatura que cobre a acao do Governo escreve Pedro Passos Coelho numa carta enviada hoje a Antonio Jose Seguro a que a agencia Lusa teve acesso."
-    tokens = nltk.word_tokenize(text)
+    inputFile = codecs.open(fileName,"rb","utf-8-sig")
+    #i = "abelhudo"
+    #text = "Antonio Joss Seguro ja leu a carta e prosseguiu a agenda do dia De momento nada mais ha a acrescentar disse a mesma fonte da direcao do PS fazendo contudo a seguinte ressalva Mas seria a primeira vez que um lider da oposicao nao estaria num encontro apos convite do primeiro ministro O primeiro ministro Pedro Passos Coelho convidou hoje o secretario geral do PS para um encontro a realizar na quarta feira de manha tendo em vista um entendimento sobre as medidas para a consolidacao orcamental Em particular torna se urgente garantir o cumprimento da execucao orcamental de 2013 e do quadro orcamental de medio prazo e concertar as medidas que garantam esse objetivo dado ate que algumas delas se encontram para alem do prazo da legislatura que cobre a acao do Governo escreve Pedro Passos Coelho numa carta enviada hoje a Antonio Jose Seguro a que a agencia Lusa teve acesso."
+    
+    #tokens = nltk.word_tokenize(text)
  
     for line in inputFile:
         splitted = line.split(";")
-        #lista = []
         DicionarioAux = {}
         DicionarioAux [splitted[1][:2]] = splitted[1][3:]
         DicionarioAux [splitted[2][:3]] = splitted[2][3:] 
         DicionarioAux [splitted[3].split("=")[0]] = splitted[3].split("=")[1]
-        DicionarioAux [splitted[4][:4]] = splitted[4][5:]
-        DicionarioAux [splitted[0].split(".")[1].split("=")[0]] = splitted[0].split(".")[1].split("=")[1] 
-        #lista.append(splitted[1][3:])
-        #lista.append(splitted[2][3:])
-        #lista.append(splitted[3].split("=")[1])
-        #lista.append(splitted[4][5:])
-        #lista.append(splitted[0].split(".")[1].split("=")[1])
-        #print lista
-      
-      
+        
+        #Sanitize 
+        ANOTsanitized = splitted[4][5:].replace("\n","").replace('\r','');
+        
+        DicionarioAux [splitted[4][:4]] = ANOTsanitized
+        DicionarioAux [splitted[0].split(".")[1].split("=")[0]] = splitted[0].split(".")[1].split("=")[1]       
         palavras[splitted[0].split(",")[0]] = DicionarioAux
         palavras[splitted[0].split(",")[1].split(".")[0]] = DicionarioAux
 
-        #print dict
-        #print dict.keys()
- 
         print palavras
-        raw_input("check")
+        #raw_input("check")
+
+    return palavras
 
 sentilexToPython()
+
+
  # for i in range(0,len(tokens)):
  #    if tokens[i] in palavras.keys():
  #      if (palavras[tokens[i]][2] == '-1'):
