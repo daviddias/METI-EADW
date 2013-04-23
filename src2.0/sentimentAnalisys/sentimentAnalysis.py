9c5ce8984d25f253311dc9f6b08e7af3c08362d8
David Dias <daviddias.p@gmail.com>
2013-04-23 15:36:02 +0100

    to merge

---
+++ src2.0/sentimentAnalisys/sentimentAnalysis.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+#1 Ir buscar um artigo com personalidades
+#(2nd phase) - Partir em frases
+#2 Analisar a polaridade
+#3 Adicionar a polaridade as personalidades desse artigo (2nd phase) - Adicionar só a polaridade de uma frase a pessoa que se encontra nela
+
+
--- src2.0/sentimentAnalisys/sentimentFlex.py
+++ src2.0/sentimentAnalisys/sentimentFlex.py
@@ -19,16 +19,16 @@ def DictionaryOfWords():
         splitted = line.split(";")
         DicionarioAux = {}
         if(len(splitted[1].split('=')) == 1):
-            DicionarioAux [splitted[1].split('=')[0]] = ''
+          DicionarioAux [splitted[1].split('=')[0]] = ''
         else:
-            DicionarioAux [splitted[1].split('=')[0]] = splitted[1].split('=')[1] 
+          DicionarioAux [splitted[1].split('=')[0]] = splitted[1].split('=')[1] 
         
         DicionarioAux [splitted[2].split('=')[0]] = splitted[2].split('=')[1]
         DicionarioAux [splitted[3].split(':')[0]] = splitted[3].split(':')[1].split('=')[1]
         if (splitted[4].split('=')[0] == 'ANOT'):
-            DicionarioAux [splitted[4].split('=')[0]] = splitted[4].split('=')[1]
+          DicionarioAux [splitted[4].split('=')[0]] = splitted[4].split('=')[1]
         else:
-            DicionarioAux [splitted[4].split('=')[0].split(':')[0] ] = splitted[4].split('=')[1]
+          DicionarioAux [splitted[4].split('=')[0].split(':')[0] ] = splitted[4].split('=')[1]
         
         palavras[splitted[0].split(",")[0]] = DicionarioAux 
         palavras[splitted[0].split(",")[1]] = DicionarioAux
@@ -37,11 +37,14 @@ def DictionaryOfWords():
 
 
 def checks(dict):
-  text1 = 'badalhoco ola es uma merda estarem sempre a bater na mesma tecla'
-  token = nltk.word_tokenize(text1)
+  #text1 = 'badalhoco ola es uma merda estarem sempre a bater na mesma tecla'
+  #token = nltk.word_tokenize(text1)
+  
+
   text = 'A Justica deve ter como pressuposto a, e nao ha Justica sem verdade. Apurar a nem sempre e facil mas sem  nao ha Justica'
   tokens = nltk.word_tokenize(text)
   result = 0
+  
   for word in palavras.keys():
     if word in text:
        
@@ -60,6 +63,8 @@ def checks(dict):
            elif palavras[word]['POL'] == '1':
               result = result + 1 
            else: result = result + 0  
+
+
   print result
   return result             
 
